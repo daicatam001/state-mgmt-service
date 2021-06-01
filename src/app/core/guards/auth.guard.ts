d656ddb3ebd30f03ca3d1d014f758565ec80daac
phamthanhtam <tampt95@gmail.com>
2021-06-01 14:32:13 +0700

    done angular service

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,54 @@
+import {Injectable} from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanActivateChild,
+  CanLoad,
+  Route, Router,
+  RouterStateSnapshot,
+  UrlSegment,
+  UrlTree
+} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../../services/auth.service';
+import {take, tap} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanActivateChild, CanLoad {
+  constructor(private authService: AuthService,
+              private router: Router) {
+
+  }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.isAuth$();
+  }
+
+  canActivateChild(
+    childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.isAuth$();
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.isAuth$();
+  }
+
+  private isAuth$(): Observable<boolean> {
+    return this.authService.isLoggedIn$.pipe(
+      take(1),
+      tap(isLoggedIn => {
+        console.log(isLoggedIn);
+        if (!isLoggedIn) {
+          this.router.navigate(['/not-auth']);
+        }
+      })
+    );
+  }
+}
